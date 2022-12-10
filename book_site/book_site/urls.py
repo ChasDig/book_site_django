7fa7c9cbdd303df911e30ea8b45c2aff55a03d85
ChasDig <chas.prog@gmail.com>
2022-12-10 17:40:17 +0700

    Push simple django project: book_site.

--- book_site/book_site/urls.py
+++ book_site/book_site/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 
 # Позволяем нашему API раздавать media при включенном DEBUG режиме:
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA    _URL, document_root=settings.MEDIA_ROOT)
